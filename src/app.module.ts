14c08cc2bcb0ade9042fb6473603e053fef97791
ninadimplere <ninad.s@impleretech.com>
2025-05-06 16:09:26 +0530

    Adds the prisma module to the service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,23 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from '@auth/auth.module';
 import { UsersModule } from '@users/users.module';
+import { GraphQLModule } from '@nestjs/graphql';
+
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true, // for code-first
+      driver: ApolloDriver,
+    }),
     AuthModule,
     UsersModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
